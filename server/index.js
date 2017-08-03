cc47041d7ab97a1fe78e883b3e9d38486946960b
danizavtz <daniel.pires@splab.ufcg.edu.br>
2017-08-03 14:10:30 -0300

    Adjusting for internacionalization

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@
 
 
     router.get('/', (req, res) => {
-        res.status(200).send('<html><head><style>body {font-family: Helvetica, Arial, Sans-Serif;margin-top: 5em; text-align: center;}h1 {font-size: 3em;}h2 {font-size: 2em} .page-wrap {width: 800px; margin: 0 auto; }</style></head><body><div class="page-wrap"><h1>Microservice login app</h1><h1>for token generation</h1><br><h2> By: Danizavtz</h2></div></body></html>');
+        res.status(200).send('<!DOCTYPE html><head><style>body {font-family: Helvetica, Arial, Sans-Serif;margin-top: 5em; text-align: center;}h1 {font-size: 3em;}h2 {font-size: 2em} .page-wrap {width: 800px; margin: 0 auto; }</style></head><body><div class="page-wrap"><h1>Microservice login app</h1><h1>for token generation</h1><br></div></body></html>');
     });
 
     module.exports = router;
--- server/services/login.service.js
+++ server/services/login.service.js
@@ -16,8 +16,8 @@
     };
 
     exports.validateLogin = (req, res, next) => {
-        req.checkBody('login', 'login é obrigatório no body da requisição').notEmpty();
-        req.checkBody('password', 'password é obrigatório no body da requisição').notEmpty();
+        req.checkBody('login', 'login is required').notEmpty();
+        req.checkBody('password', 'password is required').notEmpty();
         req.getValidationResult().then((result) => {
             if (!result.isEmpty()) {
                 return res.status(400).json({errors: result.array()});       
@@ -32,14 +32,14 @@
         const sql = 'SELECT e.employee_id, e.login, e.password FROM employee e WHERE e.login=$1';
         postgres.client.query(sql, [req.body.login], (err, result) => {
             if (err) {
-                return res.status(500).json({ errors: ['Não foi possível efetuar login'] });
+                return res.status(500).json({ errors: ['Could not do login'] });
             }
             if (result.rows.length === 0) {
-                return res.status(404).json({ errors: ['Usuário ou senha incorretos'] });
+                return res.status(404).json({ errors: ['User or password does not match'] });
             }
 
             if (!(scrypt.verifyKdfSync(result.rows[0].password, req.body.password.toString()))) {
-                return res.status(404).json({ errors: ['Usuário ou senha incorretos'] });
+                return res.status(404).json({ errors: ['User or password does not match'] });
             }
 
             req.employee = result.rows[0];
