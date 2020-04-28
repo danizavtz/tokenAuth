f10c9dd4a10c524a73692000afabf54b3f06073b
danizavtz <daniellucena@yahoo.com.br>
2020-04-28 12:43:30 -0300

    Fix lint errors

--- server/routes/login.route.js
+++ server/routes/login.route.js
@@ -1,7 +1,7 @@
-  const router = require('express').Router();
-  const loginService = require('../controllers/login.controller');
-  const loginValidator = require('../validators/login.validator');
+const router = require('express').Router();
+const loginService = require('../controllers/login.controller');
+const loginValidator = require('../validators/login.validator');
 
-  router.post('/login', loginValidator.validationBodyRules, loginValidator.checkRules, loginService.hashPassword, loginService.lookupLogin, loginService.logEmployee);
+router.post('/login', loginValidator.validationBodyRules, loginValidator.checkRules, loginService.hashPassword, loginService.lookupLogin, loginService.logEmployee);
 
-  module.exports = router;
+module.exports = router;
