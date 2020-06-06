a25977a468352c426b0f2b985de5c82d8895c7cd
danizavtz <daniellucena@yahoo.com.br>
2020-06-06 19:30:58 -0300

    Inserting api docs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
     const router = require('express').Router();
+    const swaggerUi = require('swagger-ui-express');
+    const swaggerDocument = require('../docs/swagger.json');
+    var options = {
+      customCss: '.swagger-ui .topbar { display: none }'
+    };
 
+    router.use('/api-docs', swaggerUi.serve);
+    router.get('/api-docs', swaggerUi.setup(swaggerDocument, options));
     router.use(require('./routes/login.route'));
     router.use((err, req, res, next) => {
         if (err.name === 'UnauthorizedError') {
